d862b02ef12ba1080b91baf4d14dbef33156e332
Pascal Molli <pascal.molli@ls2n.fr>
2025-04-04 18:04:40 +0200

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+const QueryEngine = require('@comunica/query-sparql').QueryEngine;
+
+async function main() {
+  const myEngine = new QueryEngine();
+  const bindingsStream = await myEngine.queryBindings(`
+    SELECT ?s ?p ?o WHERE {
+      ?s ?p ?o
+    } LIMIT 1`, {
+    sources: [ 'https://dbpedia.org/sparql' ],
+  });
+  
+  // Consume results as a stream (best performance)
+  bindingsStream.on('data', (binding) => {
+    console.log(binding.toString()); // Quick way to print bindings for testing
+  
+  //   console.log(binding.has('s')); // Will be true
+  
+  //   // Obtaining values
+  //   console.log(binding.get('s').value);
+  //   console.log(binding.get('s').termType);
+  //   console.log(binding.get('p').value);
+  //   console.log(binding.get('o').value);
+   });
+  bindingsStream.on('end', () => {
+    // The data-listener will not be called anymore once we get here.
+  });
+  bindingsStream.on('error', (error) => {
+    console.error(error);
+  });
+  
+  // Consume results as async iterable (easier)
+  for await (const binding of bindingsStream) {
+    console.log(binding.toString());
+  }
+  
+  // // Consume results as an array (easier)
+  // const bindings = await bindingsStream.toArray();
+  // console.log(bindings[0].get('s').value);
+  // console.log(bindings[0].get('s').termType);
+}
+
+main();
